f070b44ebc3c594f6665a533c7da5a7543e15e19
Dmitriy3292 <mr.diy@yandex.ru>
2021-11-07 12:41:06 +0400

    first commit

---
+++ src/main/java/ru/netology/sqr/sqrService.java
@@ -0,0 +1,19 @@
+package ru.netology.sqr;
+
+public class sqrService {
+    public int sqrt(int down, int up) {
+
+        int counter = 0;
+        for (int i = 10; i <= 99 ; i++) {
+            if (i * i >= down && i * i <= up) {
+
+
+            }
+
+
+        }
+return 0;
+
+    }
+
+}
---
+++ src/test/java/ru/netology/sqr/SqrServiceTest.java
@@ -0,0 +1,17 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class sqrServiceTest {
+
+    @Test
+    void shouldsqrt() {
+        sqrService sqrService = new sqrService();
+
+        int expected = 3;
+                int actual = sqrService.sqrt(200,300);
+        assertEquals(expected,actual);
+    }
+}
